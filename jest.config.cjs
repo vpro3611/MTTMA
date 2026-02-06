bfc6b16e120fdf8c5c02b262994c03d48240dad0
vpro3611 <vprov8645@gmail.com>
2026-02-06 21:47:25 +0100

    Config: jest's confing

---
+++ jest.config.cjs
@@ -0,0 +1,28 @@
+const { createDefaultPreset } = require("ts-jest");
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+
+  extensionsToTreatAsEsm: ['.ts'],
+
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+
+  // setupFilesAfterEnv: ['<rootDir>/__tests__/integration_tests/setup.ts'],
+
+  testMatch: [
+    '**/__tests__/**/*.spec.ts',
+    '**/?(*.)+(spec|test).ts'
+  ],
+
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+
+  verbose: true,
+};
