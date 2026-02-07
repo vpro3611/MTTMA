4838b1d550dea96f027f2a6fe5181a50d88cf7b0
vpro3611 <vprov8645@gmail.com>
2026-02-07 23:10:44 +0100

    Update: updated modules and dependencies created for organization

--- src/container.ts
+++ src/container.ts
@@ -5,11 +5,14 @@ import {PasswordHasher} from "../src/modules/user/application/ports/password_has
 import {ChangePasswordUseCase} from "../src/modules/user/application/change_pass_use_case.js";
 import {ChangeUserEmailUseCase} from "../src/modules/user/application/change_user_email_use_case.js";
 import {RegisterUseCase} from "../src/modules/user/application/register_use_case.js";
+import {OrganizationRepositoryPG} from "./modules/organization/repository_realization/organization_repository.js";
+import {CreateOrganizationUseCase} from "./modules/organization/application/create_organization_use_case.js";
 
 export function assembleContainer() {
     // repositories
     const userRepoPG = new UserRepositoryPG(pool);
 
+    const organizationRepoPG = new OrganizationRepositoryPG(pool);
     // infrastructure services
     const hasher: PasswordHasher = new HasherBcrypt();
 
@@ -17,6 +20,9 @@ export function assembleContainer() {
     const changePassUC = new ChangePasswordUseCase(userRepoPG, hasher);
     const changeEmailUC = new ChangeUserEmailUseCase(userRepoPG);
     const registerUC = new RegisterUseCase(userRepoPG, hasher);
+
+    const createOrganizationUC = new CreateOrganizationUseCase(organizationRepoPG);
+    const renameOrganizationUC = new CreateOrganizationUseCase(organizationRepoPG);
 
 
 }
