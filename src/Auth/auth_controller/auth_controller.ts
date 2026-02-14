1ecd48c2ca7aa48b9c1878b596781d5870cc296a
vpro3611 <vprov8645@gmail.com>
2026-02-14 22:16:12 +0100

    Refactor: refactored refresh and logout

--- src/Auth/auth_controller/auth_controller.ts
+++ src/Auth/auth_controller/auth_controller.ts
@@ -1,5 +1,6 @@
 import {AuthService} from "../auth_service/auth_service.js";
 import {Request, Response} from "express";
+import {MissingRefreshTokenError} from "../../http_errors/token_errors.js";
 
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
@@ -8,28 +9,22 @@ export class AuthController {
         const refreshToken = req.cookies.refreshToken;
 
         if (!refreshToken) {
-            return res.status(401).json({message: "No refresh token provided"});
+            throw new MissingRefreshTokenError();
         }
 
-        try {
-            const tokens = await this.authService.refresh(refreshToken);
 
-            res
-                .cookie("refreshToken", tokens.refreshToken,
-                    {
-                        httpOnly: true,
-                        secure: process.env.NODE_ENV! === "production",
-                        sameSite: process.env.NODE_ENV! === "production" ? "strict" : "lax",
-                        maxAge: 7 * 24 * 60 * 60 * 1000,
-                    })
-                .json({accessToken: tokens.accessToken});
-        } catch (e: unknown) {
-            if (e instanceof Error) {
-                res.status(401).json({message: e.message});
-            }
-            res.status(500).json({message: "Internal server error"});
-        }
-    }
+        const tokens = await this.authService.refresh(refreshToken);
+
+        res
+            .cookie("refreshToken", tokens.refreshToken,
+                {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV! === "production",
+                    sameSite: process.env.NODE_ENV! === "production" ? "strict" : "lax",
+                    maxAge: 7 * 24 * 60 * 60 * 1000,
+                })
+            .json({accessToken: tokens.accessToken});
+    };
 
     register = async (req: Request, res: Response) => {
         const {email, password} = req.body;
@@ -75,7 +70,19 @@ export class AuthController {
 
     logout = async (req: Request, res: Response) => {
         const refreshToken = req.cookies.refreshToken;
-        const message = await this.authService.logout(refreshToken);
-        res.clearCookie("refreshToken").sendStatus(204);
+
+        if (!refreshToken) {
+            throw new MissingRefreshTokenError();
+        }
+
+        await this.authService.logout(refreshToken);
+
+        res.clearCookie("refreshToken", {
+            httpOnly: true,
+            secure: process.env.NODE_ENV! === "production",
+            sameSite: process.env.NODE_ENV! === "production" ? "strict" : "lax",
+        });
+
+        res.status(204).send();
     }
 }
