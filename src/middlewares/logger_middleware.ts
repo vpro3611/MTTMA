5b13d2c904fbaf38f3d2b5aa464fdaa2f1002197
vpro3611 <vprov8645@gmail.com>
2026-02-15 21:37:59 +0100

    Change: changed date representation in logger middleware

--- src/middlewares/logger_middleware.ts
+++ src/middlewares/logger_middleware.ts
@@ -3,7 +3,8 @@ import {NextFunction, Request, Response} from "express";
 
 export const loggerMiddleware = () => {
     return (req: Request, res: Response, next: NextFunction) => {
-        console.log(`Request: ${req.method} ${req.originalUrl} -> [${Date.now().toString()}]`)
+        const timestamp = new Date().toISOString();
+        console.log(`Request: ${req.method} ${req.originalUrl} -> [${timestamp}]`)
         next();
     }
 }
