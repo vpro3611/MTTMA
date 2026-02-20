93b5a18e73aeb82d3eb39edd782e54bffce4117e
vpro3611 <vprov8645@gmail.com>
2026-02-20 21:14:48 +0100

    Change: changed port

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ export async function startServer() {
 
     const app = createApp(dependencies);
 
-    const port = process.env.PORT || 3333;
+    const port = process.env.PORT || 3000;
 
     app.listen(port, () => console.log(`Server listening on port ${port}`));
 }
