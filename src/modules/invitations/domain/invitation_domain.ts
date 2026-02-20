6b165fa9accb3dada477c4c4ff5db1c10fa13082
vpro3611 <vprov8645@gmail.com>
2026-02-20 03:17:34 +0100

    Add: added new invitations status and added a couple of new methods for invitations domain model

--- src/modules/invitations/domain/invitation_domain.ts
+++ src/modules/invitations/domain/invitation_domain.ts
@@ -1,5 +1,6 @@
 import {OrgMemsRole} from "../../organization_members/domain/org_members_role.js";
 import {InvitationStatus} from "./invitation_status.js";
+import {InvitationNotPendingError} from "../errors/domain_errors.js";
 
 
 export class Invitation {
@@ -14,14 +15,23 @@ export class Invitation {
         private expiredAt: Date,
     ) {}
 
+    ensureIsPending() {
+         if (this.status !== InvitationStatus.PENDING) {
+             throw new InvitationNotPendingError();
+         }
+    }
+
+    private static calculateExpiration(): Date {
+         const date = new Date();
+         date.setDate(date.getDate() + 7);
+         return date;
+    }
+
     static createInvitation(invitation : {
         organization_id: string,
         invited_user_id: string,
         invited_by_user_id: string,
         role: OrgMemsRole,
-        status: InvitationStatus,
-        createdAt: Date,
-        expiredAt: Date,
     }) {
          return new Invitation(
              crypto.randomUUID(),
@@ -29,12 +39,16 @@ export class Invitation {
              invitation.invited_user_id,
              invitation.invited_by_user_id,
              invitation.role,
-             invitation.status,
-             invitation.createdAt,
-             invitation.expiredAt,
+             "PENDING",
+             new Date(),
+             this.calculateExpiration(),
          );
     }
 
+    setStatus(status : InvitationStatus) {
+         this.ensureIsPending();
+         this.status = status;
+    }
      getOrganizationId = () =>  this.organization_id;
      getInvitedUserId = () =>  this.invited_user_id;
      getInvitedByUserId = () =>  this.invited_by_user_id;
--- src/modules/invitations/domain/invitation_status.ts
+++ src/modules/invitations/domain/invitation_status.ts
@@ -6,6 +6,7 @@ export const InvitationStatus = {
     REJECTED: 'REJECTED',
     ACCEPTED: 'ACCEPTED',
     EXPIRED: 'EXPIRED',
+    CANCELED: 'CANCELED',
 } as const;
 
 export type InvitationStatus = typeof InvitationStatus[keyof typeof InvitationStatus];
