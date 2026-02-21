493d8e236731df0856dbaa273daa2503e878edd5
vpro3611 <vprov8645@gmail.com>
2026-02-21 02:30:31 +0100

    Changed some things in here and added controller for listing tasks

--- src/modules/organization_task/domain/task_status.ts
+++ src/modules/organization_task/domain/task_status.ts
@@ -3,7 +3,7 @@ export const TaskStatus = {
     TODO: "TODO",
     COMPLETED: "COMPLETED",
     IN_PROGRESS: "IN_PROGRESS",
-    CANCELLED: "CANCELLED",
+    CANCELLED: "CANCELED",
 } as const;
 
 export type TaskStatus = typeof TaskStatus[keyof typeof TaskStatus];
