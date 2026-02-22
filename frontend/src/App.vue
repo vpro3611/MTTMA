378a7b3ffeac52c8220aabc4e8adb048fec0b5ac
vpro3611 <vprov8645@gmail.com>
2026-02-22 22:10:33 +0100

    Add: add new stuff to app and added support of router in main

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,56 +1,29 @@
 <script setup lang="ts">
-
-import Register from "./pages/Register.vue";
-import Login from "./pages/Login.vue";
-import Logout from "./pages/Logout.vue";
-import {computed, onMounted} from "vue";
-import {authStore} from "./stores/auth_store.ts";
-import {authApi} from "./api/auth.ts";
+import { onMounted } from "vue";
+import { authStore } from "./stores/auth_store";
+import { authApi } from "./api/auth";
 
 onMounted(async () => {
-  const refreshData = await authApi.refresh();
-  if (!refreshData?.accessToken) {
-    return;
+  try {
+    const refreshData = await authApi.refresh();
+
+    if (refreshData?.accessToken) {
+      authStore.setToken(refreshData.accessToken, null);
+      const user = await authApi.me();
+      if (!user) {
+        authStore.clearToken();
+        return;
+      }
+      authStore.setUser(user);
+    }
+  } catch (e) {
+    authStore.clearToken();
+  } finally {
+    authStore.isBootstrapping = false;
   }
-  authStore.accessToken = refreshData.accessToken;
-
-  const user = await authApi.me();
-  if (!user) {
-    return;
-  }
-  authStore.user = user;
-})
-
-const isAuthenticated = computed(() => !!authStore.user?.email);
-
+});
 </script>
 
 <template>
-  <div v-if="!isAuthenticated">
-    <h1>Register</h1>
-    <Register />
-
-    <h1>Login</h1>
-    <Login />
-  </div>
-
-  <div v-else>
-    <h1>Welcome {{ authStore.user?.email }}</h1>
-    <Logout />
-  </div>
+  <router-view />
 </template>
-
-<!--<style scoped>-->
-<!--.logo {-->
-<!--  height: 6em;-->
-<!--  padding: 1.5em;-->
-<!--  will-change: filter;-->
-<!--  transition: filter 300ms;-->
-<!--}-->
-<!--.logo:hover {-->
-<!--  filter: drop-shadow(0 0 2em #646cffaa);-->
-<!--}-->
-<!--.logo.vue:hover {-->
-<!--  filter: drop-shadow(0 0 2em #42b883aa);-->
-<!--}-->
-<!--</style>-->
