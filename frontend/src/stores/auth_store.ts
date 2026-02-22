20dc9c61d6406b0a1886a4329dcd5ee94ba6f423
vpro3611 <vprov8645@gmail.com>
2026-02-22 22:09:40 +0100

    Change: changed auth store

--- frontend/src/stores/auth_store.ts
+++ frontend/src/stores/auth_store.ts
@@ -1,21 +1,24 @@
-import {reactive} from "vue";
-import type {User} from "../types/auth_types.ts";
-
-
-// const accessToken = ref<string | null>(null);
-// const user = ref<any>(null);
+import { reactive } from "vue";
+import type { User } from "../types/auth_types";
 
 export const authStore = reactive({
     accessToken: null as string | null,
     user: null as User | null,
+    isBootstrapping: true,
 
-    setToken(token: string, userData: User) {
+    setToken(token: string, user: User | null) {
         this.accessToken = token;
-        this.user = userData;
+        if (user) {
+            this.user = user;
+        }
+    },
+
+    setUser(user: User) {
+        this.user = user;
     },
 
     clearToken() {
         this.accessToken = null;
         this.user = null;
     }
-})
+});
