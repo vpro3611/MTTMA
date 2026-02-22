e5d58f4d788ee1d896395e5f5b864edb5a765b63
vpro3611 <vprov8645@gmail.com>
2026-02-22 23:48:01 +0100

    Add: layots global

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Navbar from "../components/Navbar.vue";
+</script>
+
+<template>
+  <div class="app">
+    <header>
+      <Navbar />
+    </header>
+
+    <main class="content">
+      <router-view />
+    </main>
+
+    <footer>
+      <p>Footer</p>
+    </footer>
+  </div>
+</template>
