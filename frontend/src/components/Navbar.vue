95a4bc954e742187950b980d9f17409b229fdd69
vpro3611 <vprov8645@gmail.com>
2026-02-22 23:48:41 +0100

    Add: added navbar

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import { authStore } from "../stores/auth_store";
+
+const router = useRouter();
+
+const goToProfile = () => {
+  if (authStore.user) {
+    router.push(`/profile/${authStore.user.id}`);
+  }
+};
+
+const goToUsers = () => {
+  router.push("/users");
+};
+</script>
+
+<template>
+  <nav>
+    <div>
+      <button @click="goToProfile">My Profile</button>
+      <button @click="goToUsers">Users</button>
+    </div>
+
+    <div>
+      <span v-if="authStore.user">
+        {{ authStore.user.email }}
+      </span>
+    </div>
+  </nav>
+</template>
