6d02dea7e79eaca82953b6493875cbb24d268ac0
vpro3611 <vprov8645@gmail.com>
2026-02-23 01:28:19 +0100

    Add: added new pages: organization view and in general for organizations

---
+++ frontend/src/pages/OrganizationsView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+import { ref, onMounted } from "vue";
+import { organizationsAPI as orgAPI } from "../api/organizations";
+
+const route = useRoute();
+const org = ref<any>(null);
+
+onMounted(async () => {
+  org.value = await orgAPI.getById(route.params.orgId as string);
+});
+</script>
+
+<template>
+  <section v-if="org">
+    <h1>{{ org.name }}</h1>
+  </section>
+
+  <div v-else>
+    Loading...
+  </div>
+</template>
