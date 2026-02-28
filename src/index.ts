79570b595661159ac0193be6828f3d0460f8e5dc
vpro3611 <vprov8645@gmail.com>
2026-02-28 19:08:34 +0100

    Refactor: refactored the main bootstrapping function so now it starts cron with server

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,32 @@
 import {startServer} from "./server.js";
 import dotenv from "dotenv";
+import {RunCron} from "./modules/cron_manager/run_cron.js";
+import {JobRunner} from "./modules/cron_manager/jobs/cron_transactional.js";
+import {TransactionManagerPg} from "./modules/transaction_manager/transaction_manager_pg.js";
+import {pool} from "./db/pg_pool.js";
+
+dotenv.config();
+
+async function bootstrap() {
+    // deps
+    const txManager = new TransactionManagerPg(pool);
+    const jobRunner = new JobRunner(txManager);
+
+    // cron related
+    const cronJob = new RunCron(jobRunner);
+    await cronJob.proceedExpiredScan();
+
+    // server starting
+    await startServer();
+}
 
 
-startServer().catch((err) => {
-    console.error("Error while starting the server: ", err);
+bootstrap().catch((err) => {
+    console.error("Error while bootstrapping: ", err);
     process.exit(1);
 })
+
+// startServer().catch((err) => {
+//     console.error("Error while starting the server: ", err);
+//     process.exit(1);
+// })
